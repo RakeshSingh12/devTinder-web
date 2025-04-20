a87062933600b5147a8341916870e4e9399b1cf9
Rakesh Singh <36115281+RakeshSingh12@users.noreply.github.com>
2025-04-20 14:14:52 +0530

    Feat: Added Signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,8 +6,11 @@ import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constant";
 const Login = () => {
-    const [email, setEmailId] = useState("atharv@gmail.com");
-    const [password, setPassword] = useState("Atharv@123");
+    const [email, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [isLoggingForm, setLoggingForm] = useState(false);
     const [error, setError] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -31,12 +34,49 @@ const Login = () => {
         }
 
     };
+    const handleSignUp = async () => {
+        try {
+            const res = await axios.post(BASE_URL + "signup", {
+                firstName,
+                lastName,
+                email,
+                password
+            }, {
+                withCredentials : true
+            })
+            // dispatch the user data
+            dispatch(addUser(response.data));
+            return navigate("/profile")
+
+        } catch (err) {
+            setError(err?.response?.data || "Something went wrong")
+        }
+    }
     return (
         <div className="flex justify-center my-10">
             <div className="card card-border bg-base-300 w-96">
                 <div className="card-body">
-                    <h2 className="card-title justify-center">Login</h2>
+                    <h2 className="card-title justify-center">{isLoggingForm ? "Login" : "Register"}</h2>
                     <div>
+                        {!isLoggingForm && <><fieldset className="fieldset m-2">
+                            <legend className="fieldset-legend">First Name</legend>
+                            <input
+                                type="text"
+                                className="input"
+                                placeholder="Please enter email ID"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)} />
+                        </fieldset>
+                            <fieldset className="fieldset m-2">
+                                <legend className="fieldset-legend">Last Name</legend>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="Please enter email ID"
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)} />
+                            </fieldset>
+                        </>}
                         <fieldset className="fieldset m-2">
                             <legend className="fieldset-legend">Email ID</legend>
                             <input
@@ -57,10 +97,10 @@ const Login = () => {
                         </fieldset>
                     </div>
                     <p className="text-center text-red-500"> {error}</p>
-                    <p className="text-center text-gray-600">Don't have an account? <a href="/register">Register</a></p>
+                    <p className="text-center text-gray-200 cursor-pointer" onClick={() => setLoggingForm((value) => !value)}>{isLoggingForm ? "Don't have an account? Signup here" : "Existing user? Login here"}</p>
 
                     <div className="card-actions justify-center p-2">
-                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                        <button className="btn btn-primary" onClick={isLoggingForm ? handleLogin : handleSignUp}>{isLoggingForm ? "Login" : "Register"}</button>
                     </div>
                 </div>
             </div>
