cf0461dd111a77f8ad460f5ff4b7337d710b1fdb
Rakesh Singh <36115281+RakeshSingh12@users.noreply.github.com>
2025-12-05 11:45:02 +0530

    chore: automatically open browser when starting development server

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/devTinder-web/"
+  base: "/devTinder-web/",
+  server: {
+    open: true,
+  },
 });
